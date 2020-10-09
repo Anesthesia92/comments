7f632e98680752b3a0930acc6eb0b242a6e68957
user <angelice92@mail.ru>
2020-10-09 16:20:17 +0300

    сократила число комментариев до 10

--- services/getComments.js
+++ services/getComments.js
@@ -10,19 +10,21 @@ const fetchPromise = fetch("https://jsonplaceholder.typicode.com/comments");
 let mainContent = '',
     main = document.querySelector('#comments .comments');
 
+
 fetchPromise.then(response => {
     return response.json();
 }).then(comments => {
-    comments.forEach(comment => {
-        mainContent+=`
+    comments.some(comment => {
+        mainContent+= `
      <div class="comment-text text-justify mt-2" id="comments">
  ${comment.name}
        <p class="comments">${comment.body}</p>
  
 
    </div>`;
-
-
+        if (comments.length > 10) {
+            comments.length = 10;
+        }
     });
     console.log(comments);
     main.innerHTML = mainContent;

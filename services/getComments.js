f944c8f81cfa6db4b50dca1f8a2fcd95fe3d095b
user <angelice92@mail.ru>
2020-10-10 13:46:50 +0300

    доделала цикл for с комментариями

--- services/getComments.js
+++ services/getComments.js
@@ -1,31 +1,48 @@
-// fetch('https://jsonplaceholder.typicode.com/comments')
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .then((data) => {
-//         console.log(data);
-//     });
-
 const fetchPromise = fetch("https://jsonplaceholder.typicode.com/comments");
-let mainContent = '',
-    main = document.querySelector('#comments .comments');
 
-
-fetchPromise.then(response => {
+fetchPromise
+  .then((response) => {
     return response.json();
-}).then(comments => {
-    for (let i = 0; i < 10; i++) {
-        mainContent+= `
-     <div class="comment-text text-justify mt-2" id="comments">
- ${comment.name}
-       <p class="comments">${comment.body}</p>
- 
-
-   </div>`;
-
-    }
-    console.log(comments);
-    main.innerHTML = mainContent;
-});
-
-
+  })
+  .then((comment) => {
+    listRendering(comment);
+  });
+function listRendering(comment) {
+  for (let i = 0; i < 10; ++i) {
+    $("#comments").append(commentBody(comment[i]));
+  }
+}
+function commentBody({ email, name, body }) {
+  return ` <div class="p-3 bg-white mt-2 rounded">
+            <div class="d-flex justify-content-between">
+              <div class="d-flex flex-row user">
+                <img
+                  class="rounded-circle img-fluid img-responsive"
+                  src="https://i.imgur.com/JXZLwEY.jpg"
+                  width="40"
+                  alt="img"
+                />
+                <div class="d-flex flex-column ml-2">
+                  <span class="font-weight-bold">${email}</span
+                  ><span class="day">2 days ago</span>
+                </div>
+              </div>
+              <div class="d-flex align-items-center px-3 heart border">
+                <i class="fa fa-heart-o heart-icon"> </i
+                ><span class="ml-2">35</span>
+              </div>
+            </div>
+            <div class="comment-text text-justify mt-2">
+             ${name}
+              <p class="comments">${body}</p>
+            </div>
+            <div class="d-flex justify-content-end align-items-center comment-buttons mt-2 text-right">
+              <span class="mr-3 delete">Delete</span
+              ><button
+                class="btn btn-success btn-sm border-0 px-3"
+                type="button">
+                Edit
+              </button>
+            </div>
+          </div>`;
+}
